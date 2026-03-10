d8745217605b091d823e415378a2bd6e8f87a15f
Cameron <23451@wc.school.nz>
2026-03-11 09:38:27 +1300

    # add start menu - DTS assignment file

--- DTS assignment.py
+++ DTS assignment.py
@@ -7,6 +7,8 @@ import random
 
 
 # ----------------------- Variables -----------------------
+
+# Dictionaries and lists
 player_stats = {
     "Health",
     "Damage",
@@ -22,11 +24,11 @@ player_equipment = {
 
 inventory = []
 
-POSSIBLE_CLASSES = {
-    "test1": {"Health":5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5},
-    "test2": {"Health":5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5},
-    "test3": {"Health":5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5}
-}
+POSSIBLE_CLASSES = [
+    {"Name": "test1", "Health": 5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5},
+    {"Name": "test2", "Health": 5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5},
+    {"Name": "test3", "Health": 5, "Damage": 5, "Defense":0.5, "Strength":0.5, "Stamina": 5}
+]
 
 POSSIBLE_WEAPONS = {
     "Sword": {
@@ -56,13 +58,13 @@ POSSIBLE_ARMOUR = {
 
 POSSIBLE_ENEMIES = {
     "test1" : {
-        "Stats" : {"Health": 10, "Stamina" : 5, "Weakness": "Melee"}
+        "Stats" : {"Name": "test1", "Health": 10, "Stamina" : 5, "Weakness": "Melee"},
         "Move 1": {"Base damage": 3, "Type" : "Melee", "Stamina use": 0},
         "Move 2": {"Base damage": 6, "Type" : "Melee", "Stamina use": 1},
         "Move 3": {"Base damage": 9, "Type" : "Melee", "Stamina use": 2}
     },
     "test2" : {
-        "Stats" : {"Health": 20, "Stamina" : 10, "Weakness": "Ranged"}
+        "Stats" : {"Name": "test2", "Health": 20, "Stamina" : 10, "Weakness": "Ranged"},
         "Move 1": {"Base damage": 6, "Type" : "Ranged", "Stamina use": 0},
         "Move 2": {"Base damage": 9, "Type" : "Ranged", "Stamina use": 2},
         "Move 3": {"Base damage": 12, "Type" : "Ranged", "Stamina use": 4}
@@ -71,22 +73,80 @@ POSSIBLE_ENEMIES = {
 
 
 
+# variables
 
 
 # ----------------------- Functions -----------------------
 def create_player_stats(player_class):
     global player_stats
-    if player_class == possible_classes["test1"]:
-        player_stats = possible_classes["test1"]
+    if player_class == POSSIBLE_CLASSES["test1"]:
+        player_stats = POSSIBLE_CLASSES["test1"]
 
-    elif player_class == possible_classes["test2"]:
-        player_stats = possible_classes["test2"]
+    elif player_class == POSSIBLE_CLASSES["test2"]:
+        player_stats = POSSIBLE_CLASSES["test2"]
 
-    elif player_class == possible_classes["test2"]:
-        player_stats = possible_classes["test2"]
+    elif player_class == POSSIBLE_CLASSES["test2"]:
+        player_stats = POSSIBLE_CLASSES["test2"]
 
-
+def enter_to_continue():
+    input("Enter to continue: ")
+    print()
 
 
 
 # ----------------------- Main code -----------------------
+
+print("Start game")
+
+while True:
+    try:
+        start_choice = str(input("yes/no: "))
+        start_choice = start_choice.lower()
+        if start_choice == "yes" or start_choice == "y":
+            start_choice = "yes"
+            break
+        elif start_choice == "no" or start_choice == "n":
+            start_choice = "no"
+            break
+        else:
+            print("Not a valid string")
+    except ValueError:
+        print("Not a valid input")
+
+if start_choice == "no":
+    print("Quitting game")
+    time.sleep(1)
+    quit()
+
+print("Welcome")
+enter_to_continue()
+
+print("Choose class")
+print()
+
+for i in range(len(POSSIBLE_CLASSES)):
+    print("Class", i+1, ":", POSSIBLE_CLASSES[i]["Name"])
+    print("Stats: ")
+    print("| Health:", POSSIBLE_CLASSES[i]["Health"], "| Damage:", POSSIBLE_CLASSES[i]["Damage"],
+          "| Defense:", POSSIBLE_CLASSES[i]["Defense"], "| Strength:", POSSIBLE_CLASSES[i]["Strength"],
+          "| Stamina:", POSSIBLE_CLASSES[i]["Stamina"], " |"
+          )
+    print()
+
+print()
+
+for i in range(len(POSSIBLE_CLASSES)):
+    print("Type", i+1, "for", POSSIBLE_CLASSES[i]["Name"])
+
+while True:
+    try:
+        player_class_choice = int(input("Choose a class: "))
+        if player_class_choice >= 1 and player_class_choice <= len(POSSIBLE_CLASSES):
+            player_class_choice = player_class_choice - 1
+            break
+        else:
+            print("Not a valid input")
+
+    except ValueError:
+        print("Not a valid input")
+
